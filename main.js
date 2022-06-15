091b45091ee29de7c6f17e6caed4d5a18da84d51
hvergote <henry.vergote@student.hogent.be>
2022-06-15 14:48:34 +0200

    navbar

--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 function burgirClick() {
-    if (document.querySelector(".burgir").id == "burgirOpen") {
-        document.querySelector(".burgir").id = "burgirClosed";
-        document.getElementById("nav").style.display = "none";
-    } else {
-        document.querySelector(".burgir").id = "burgirOpen";
-        document.getElementById("nav").style.display = "block";
+// check if screen is 768px or less
+    if (window.innerWidth <= 768) {
+        if (document.querySelector(".burgir").id == "burgirOpen") {
+            document.querySelector(".burgir").id = "burgirClosed";
+            document.getElementById("nav").style.display = "none";
+        } else {
+            document.querySelector(".burgir").id = "burgirOpen";
+            document.getElementById("nav").style.display = "block";
+        }
     }
 }

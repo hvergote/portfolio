46caa33a4046547a0e67a4a415ab0afe49d05b71
Henry Vergote <henry.vergote@student.hogent.be>
2022-11-14 19:54:01 +0100

    navbar

---
+++ portfolio/src/components/Navbar.jsx
@@ -0,0 +1,16 @@
+import '../css/nav.css'
+
+export default () => {
+    return (
+
+        <div>
+        <nav>
+            <h1>Henry Vergote</h1>
+            <ul>
+                <li><a href="#">About me</a></li>
+                <li><a href="#">Contact</a></li>
+                <li><a href="#">Skills</a></li>
+            </ul>
+        </nav>
+    </div>
+)}

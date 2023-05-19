ec8550201b58e4070ce8dda6ff5c1749ef318a19
Henry Vergote <henry.vergote@student.hogent.be>
2023-05-19 10:21:05 +0200

    github profile update

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -8,7 +8,7 @@ export default () => {
         <div id='sidediv'>
             <ul id='sidelist'>
                 <li><a href="mailto:henry.vergote@hotmail.be"><IoMail class="icon" id="mail"/></a></li>
-                <li><a href="https://github.com/henryvergote" target="_blank"><IoLogoGithub class="icon" id='git'/></a></li>
+                <li><a href="https://github.com/hvergote" target="_blank"><IoLogoGithub class="icon" id='git'/></a></li>
                 <li><a href="https://www.linkedin.com/in/henry-vergote-989ab8207/" target="_blank"><IoLogoLinkedin class="icon" id='linked'/></a></li>
                 <li><a href="https://soundcloud.com/ryverbe" target="_blank"><IoLogoSoundcloud class="icon" id='sound'/></a></li>
             </ul>

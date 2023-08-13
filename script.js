58ae7ad7c6a1f2350466a6eb039db23d947e75c1
Henry Vergote <henry.vergote@student.hogent.be>
2023-08-13 18:24:53 +0200

    site

---
+++ script.js
@@ -0,0 +1,31 @@
+window.onload = function() {
+    if (window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches) {
+        loadColorScheme(1);
+    } else {
+        loadColorScheme(0);
+    }
+}
+
+const loadColorScheme = (scheme) => {
+    var head = document.getElementsByTagName('head')[0];
+    var link = document.createElement('link');
+    link.rel = 'stylesheet';
+    link.type = 'text/css';
+    if (scheme == 1) {
+        link.href = 'style_light.css';
+    }
+    else {
+        link.href = 'style_dark.css';
+    }
+    head.appendChild(link);
+}
+
+window.addEventListener('scroll', function() {
+    var backToTopButton = document.querySelector('.backToTop');
+    
+    if (window.scrollY > 300) {
+        backToTopButton.style.display = 'block';
+    } else {
+        backToTopButton.style.display = 'none';
+    }
+});

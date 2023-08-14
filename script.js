3e93950598c6b4aa9e4bbd5ec76465f8cda630e6
Henry Vergote <henry.vergote@student.hogent.be>
2023-08-14 11:56:38 +0200

    mobile optimization

--- script.js
+++ script.js
@@ -5,11 +5,26 @@ window.onload = function() {
     } else {
         loadColorScheme(0);
     }
+    const navLinks = document.querySelectorAll('.navitem');
+        if (window.innerWidth < 600) {
+            navLinks.forEach(function(link) {
+                link.addEventListener('click', function() {
+                    document.getElementById('burgers').style.display = 'none';
+                    document.getElementById('hamburger').style.display = 'flex';
+                });
+            });
+        }
+
+    document.getElementById('hamburger').addEventListener('click', function() {
+        document.getElementById('burgers').style.display = 'block';
+        document.getElementById('hamburger').style.display = 'none';
+        document.getElementById('navlist').style.backdropFilter = 'blur(5px)';
+    });
 }
 
 const loadColorScheme = (scheme) => {
-    var head = document.getElementsByTagName('head')[0];
-    var link = document.createElement('link');
+    const head = document.getElementsByTagName('head')[0];
+    const link = document.createElement('link');
     link.rel = 'stylesheet';
     link.type = 'text/css';
     if (scheme == 1) {

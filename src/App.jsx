f51cdd15d5925a7c05f9d0b688e4850426388b3d
hvergote <henry.vergote@student.hogent.be>
2024-01-16 22:33:41 +0100

    init v3?

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,34 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
 import './App.css'
-import Navbar from './components/Navbar'
-import Sidebar from './components/Sidebar'
-import Home from './components/Home'
-import About from './components/About'
-import ScrollToTop from "react-scroll-to-top";
-import Skills from './components/Skills'
-import Contact from './components/Contact'
-
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <Navbar />
-      <Sidebar />
-      <ScrollToTop id='scrollbtn'/>
+    <>
       <div>
-        <Home />
-        <About />
-        <Skills />
-        <Contact />
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
       </div>
-    </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
   )
 }
 

4b7157f0c0bed9b1be168e5fc9c9fbeb73785fbd
Henry Vergote <henry.vergote@student.hogent.be>
2024-01-23 15:19:44 +0100

    hashrouter

--- src/main.jsx
+++ src/main.jsx
@@ -2,12 +2,40 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import { RouterProvider, createBrowserRouter, createHashRouter } from 'react-router-dom'
 import About from './components/about.jsx'
 import Error from './components/error.jsx'
 import Skills from './components/skills.jsx'
 import Home from './components/home.jsx'
 import Contact from './components/contact.jsx'
+
+const hashRouter = createHashRouter([
+  {
+    path: '/',
+    exact: true,
+    element: <App />,
+  },
+  {
+    path: '/home',
+    element: <Home />,
+  },
+  {
+    path: '/about',
+    element: <About />,
+  },
+  {
+    path: '/skills',
+    element: <Skills />,
+  },
+  {
+    path: '/contact',
+    element: <Contact />,
+  },
+  {
+    path: '*',
+    element: <Error />,
+  }
+])
 
 const router = createBrowserRouter([
   {
@@ -39,6 +67,6 @@ const router = createBrowserRouter([
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={hashRouter} />
   </React.StrictMode>,
 )
